ef00d8dfae8daa3ac52a803db524b6a153f04be7
alexandrossilva <alexandrossilva@gmail.com>
2025-04-30 17:36:31 -0300

    Fontes apresentados em aula do dia 28/04/2025

---
+++ aulas/aula-07/pagina-02.js
@@ -0,0 +1,12 @@
+let n1, n2;
+let soma;
+
+n1 = window.prompt("Digite o primeiro número");
+n2 = window.prompt("Digite o segundo número");
+
+n1 = parseInt(n1);
+n2 = parseInt(n2);
+
+soma = n1 + n2;
+
+window.alert("Soma: " + soma)
